06cbfd354ba125beb2569c037d3d1419cf795abd
rowaidsayyed <rowaidsayyed@gmail.com>
2020-05-17 18:48:25 +0300

    finish lab 1

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+const Input = require('./lib/input');
+const Notes = require('./lib/notes');
+
+const arg = new Input();
+// eslint-disable-next-line no-unused-vars
+const note = new Notes(arg);
+
