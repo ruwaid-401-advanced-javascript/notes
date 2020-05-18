31507d9d396d1701aca5a9531511c835926e4d7c
rowaidsayyed <rowaidsayyed@gmail.com>
2020-05-18 23:47:39 +0300

    fix notes test

--- __test__/notes.test.js
+++ __test__/notes.test.js
@@ -7,17 +7,17 @@ jest.spyOn(global.console, 'log');
 describe('Notes Module', ()=> {
 
   it('execute() with wrong method', () =>{
-    let options = new Notes({method:'method error: i cant read your mind man :P write the method u want to use'});
+    new Notes({method:'method error: i cant read your mind man :P write the method u want to use'});
     expect(console.log).toHaveBeenCalled();
   });
 
   it('execute() without msg', () =>{
-    let options = new Notes({method:'msg error: ops u forget to write msg'});
+    new Notes({method:'msg error: ops u forget to write msg'});
     expect(console.log).toHaveBeenCalled();
   });
 
   it('execute() with correct method and a msg', () =>{
-    let options = new Notes({method:{action: 'add', payload: 'hiii'}});
+    new Notes({method:{action: 'add', payload: 'hiii'}});
     expect(console.log).toHaveBeenCalled();
   });
 
