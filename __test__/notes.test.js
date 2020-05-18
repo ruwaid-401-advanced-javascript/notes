9ebff1150f2c65db5db844da9eff18c62bc1ccb6
rowaidsayyed <rowaidsayyed@gmail.com>
2020-05-18 16:19:06 +0300

    finish lab 2

---
+++ __test__/notes.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Notes = require('../lib/notes.js');
+
+jest.spyOn(global.console, 'log');
+
+describe('Notes Module', ()=> {
+
+  it('execute() with wrong method', () =>{
+    let options = new Notes({method:'method error: i cant read your mind man :P write the method u want to use'});
+    expect(console.log).toHaveBeenCalled();
+  });
+
+  it('execute() without msg', () =>{
+    let options = new Notes({method:'msg error: ops u forget to write msg'});
+    expect(console.log).toHaveBeenCalled();
+  });
+
+  it('execute() with correct method and a msg', () =>{
+    let options = new Notes({method:{action: 'add', payload: 'hiii'}});
+    expect(console.log).toHaveBeenCalled();
+  });
+
+});
