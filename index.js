f3e48ff49c468f62e227eefe80a84e438409080f
rowaidsayyed <rowaidsayyed@gmail.com>
2020-05-19 16:02:31 +0300

    update schema

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 'use strict';
 const Input = require('./lib/input');
 const Notes = require('./lib/notes');
+// const mongoose = require('mongoose');
+// const NotesSchema = require('./models/notes-schema');
+
+// // connecting to my DB 
+// const MONGOOSE_URI = 'mongodb://localhost:27017/notes';
+// // mongo docs says to stick to this
+// mongoose.connect(MONGOOSE_URI, {userNewUrlParse: true, useUnifiedTopology: true} );
+
 
 const arg = new Input();
 // eslint-disable-next-line no-unused-vars
