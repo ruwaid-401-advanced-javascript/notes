daa596a439544c2da394f11e8dd28f5dee48f2fb
rowaidsayyed <rowaidsayyed@gmail.com>
2020-05-22 22:51:19 +0300

    finish lab 4 after refacored

--- __test__/input.test.js
+++ __test__/input.test.js
@@ -1,50 +1,25 @@
 'use strict';
 
 const Input = require('../lib/input.js');
-
+// jest.spyOn(process, 'exit');
 describe('Input Module', () => {
-
-  it('getMethods() with wrong input (when there is no method or invalid)', () => {
-    let options = new Input();
-    expect(options.getMethod('')).toEqual('method error: i cant read your mind man :P write the method u want to use');
-    expect(options.getMethod('-b')).toEqual('method error: i cant read your mind man :P write the method u want to use');
-  });
-
-  it('getMethods() with correct method but without msg', () => {
-    let options = new Input();
-    expect(options.getMethod({ a: true })).toEqual('msg error: ops u forget to write msg');
-  });
-
   it('getMethods() with correct method and a msg', () => {
     let options = new Input();
-    expect(options.getMethod({ a: 'hiii' })).toEqual({ action: 'add', payload: 'hiii' });
-    expect(options.getMethod({ add: 'woow' })).toEqual({ action: 'add', payload: 'woow' });
-    expect(options.getMethod({ delete: '5846845154987' })).toEqual({ action: 'delete', id: '5846845154987' });
-    expect(options.getMethod({ list: true })).toEqual({ action: 'list', category: true });
-    expect(options.getMethod({ d: '5846845154987' })).toEqual({ action: 'delete', id: '5846845154987' });
-    expect(options.getMethod({ l: true })).toEqual({ action: 'list', category: true });
-    expect(options.getMethod({ update: '5846845154987',m:'test new msg' })).toEqual({ action: 'update', payload: 'test new msg', id: '5846845154987' });
-  });
+    expect(options.getMethod({ add: 'test note', category: 'test category' }))
+      .toEqual({ action: 'add', category: 'test category', id: undefined, payload: 'test note', error: null });
+    expect(options.getMethod({ list: true }))
+      .toEqual({ action: 'list', category: true, id: undefined, payload: undefined, error: null });
+    expect(options.getMethod({ delete: '65165156145165' }))
+      .toEqual({ action: 'delete', category: undefined, id: '65165156145165', payload: undefined, error: null });
+    expect(options.getMethod({ update: '54846516551167', newnote: 'test new note' }))
+      .toEqual({ action: 'update', category: undefined, id: '54846516551167', payload: 'test new note', error: null });
 
-  it('getMsg() without a msg', () => {
-    let options = new Input();
-    expect(options.getMsg(true)).toEqual('msg error: ops u forget to write msg');
+    expect(options.getMethod({ add: 'test note 2' }))
+      .toEqual({ action: 'add', category: undefined, id: undefined, payload: 'test note 2', error: 'category error: forget to add category' });
+    expect(options.getMethod({ add: 'test note 3', category: true }))
+      .toEqual({ action: 'add', category: true, id: undefined, payload: 'test note 3', error: 'category error: forget to add category' });
+    expect(options.getMethod({ add: true }))
+      .toEqual({ action: 'add', category: undefined, id: undefined, payload: true, error: 'note error: ops u forget to write note' });
+    expect(options.getMethod({ help: true })).toEqual();
   });
-
-  it('getMsg() without a msg', () => {
-    let options = new Input();
-    expect(options.getMsg()).toEqual('msg error: ops u forget to write msg');
-  });
-
-  it('getMsg() with a msg', () => {
-    let options = new Input();
-    expect(options.getMsg('woow')).toEqual({ action: 'add', payload: 'woow' });
-  });
-
-  it('newMsg() with a new msg', () => {
-    let options = new Input();
-    expect(options.newMsg('test new msg','5846845154987')).toEqual({ action: 'update', payload: 'test new msg', id: '5846845154987' });
-    expect(options.newMsg()).toEqual('msg error: ops u forget to write the new msg');
-  });
-  
 });
--- __test__/notes.test.js
+++ __test__/notes.test.js
@@ -5,45 +5,36 @@ const Notes = require('../lib/notes.js');
 jest.spyOn(global.console, 'log');
 
 describe('Notes Module', () => {
-
-  it('execute() with wrong method', () => {
+  it('execute() with  error', () => {
     let options = new Notes();
-    let input = { method: 'method error: i cant read your mind man :P write the method u want to use' };
+    let input = { action: 'add', category: undefined, id: undefined, payload: 'test note 2', error: 'forget to add category' };
     options.execute(input);
     expect(console.log).toHaveBeenCalled();
   });
-
-  it('execute() without msg', () => {
+  it('execute() with  add  method and a msg', () => {
     let options = new Notes();
-    let input = { method: 'msg error: ops u forget to write msg' };
-    options.execute(input);
-    expect(console.log).toHaveBeenCalled();
-  });
-
-  it('execute() with correct method and a msg', () => {
-    let options = new Notes();
-    let input = { method: { action: 'add', payload: 'hiii' } };
+    let input = { action: 'add', payload: 'hiii' };
     options.execute(input);
     expect(console.log).toHaveBeenCalled();
   });
 
   it('execute() with delete method ', () => {
     let options = new Notes();
-    let input = { method: { action: 'delete', id: '5ec42429d7c4e1070fc613c6' } };
+    let input = { action: 'delete', id: '5ec42429d7c4e1070fc613c6' };
     options.execute(input);
     expect(console.log).toHaveBeenCalled();
   });
 
   it('execute() with list method ', () => {
     let options = new Notes();
-    let input = { method: { action: 'list', category: 'school' } };
+    let input = { action: 'list', category: 'school' };
     options.execute(input);
     expect(console.log).toHaveBeenCalled();
   });
 
   it('execute() with update method ', () => {
     let options = new Notes();
-    let input = { method: { action: 'update',payload: 'hiii', id: '5ec42429d7c4e1070fc613c6' } };
+    let input = { action: 'update', payload: 'hiii', id: '5ec42429d7c4e1070fc613c6' };
     options.execute(input);
     expect(console.log).toHaveBeenCalled();
   });
